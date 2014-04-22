5ab7534b6aaba7a3ebfe21d490d280090ac2be89
Matt H <m.hermes01@gmail.com>
2014-04-21 19:31:06 -0500

    Initial Commit with Source

---
+++ LoLStats-Backend/IndexModule.cs
@@ -0,0 +1,12 @@
+﻿namespace LoLStats_Backend
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
